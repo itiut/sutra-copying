6ace6c833c94fd73e1d96542edafc6f38af1a27e
Yuichi Tanikawa <t.yuichi111@gmail.com>
2012-09-10 10:28:18 +0900

    FullGra: Test DrawTriangleSlowAA

---
+++ fullscratch_graphics_programming/test_DrawTriangleAA.cpp
@@ -0,0 +1,40 @@
+/* -*- compile-command: "g++ -Wall -Wextra -O2 test_DrawTriangleAA.cpp lib/img32.cpp lib/imgdib.cpp lib/blt.cpp lib/graphics.cpp -lm" -*- */
+#include <iostream>
+#include <cstdio>
+#include "lib/imgdib.h"
+#include "lib/graphics.h"
+using namespace std;
+
+void Test_DrawTriangleSlowAA () {
+    CImageDIB dib(300, 300);
+    dib.PixelFill(0, 0, 300, 300, 0xffffffff, 255);
+
+    CVector2 p[3];
+    p[0].x =  30; p[0].y = 180;
+    p[1].x = 180; p[1].y =  60;
+    p[2].x = 270; p[2].y = 270;
+
+    for (int y = 0; y < 300; y++) {
+        for (int x = 0; x < 300; x++) {
+            int c = 0;
+
+            for (int dy = 0; dy < 5; dy++) {
+                for (int dx = 0; dx < 5; dx++) {
+                    if (InTriangle(x + dx / 5.0 , y + dy / 5.0, &p[0], &p[1], &p[2])) {
+                        c++;
+                    }
+                }
+            }
+
+            if (c > 0) {
+                dib.PixelSet(x, y, 0xff000000, 255 * c / 25);
+            }
+        }
+    }
+
+    dib.WriteBitmap("output_DrawTriangleSlowAA.bmp");
+}
+
+int main () {
+    Test_DrawTriangleSlowAA();
+}
