4d3319780a17c9a7ff0012e2d5127111e3bc6925
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-09-27 17:53:47 +0900

    meta-ruby: section 4.4

---
+++ metaprogramming-ruby/c04/eigenclass.rb
@@ -0,0 +1,40 @@
+class Object
+  def eigenclass
+    class << self
+      self
+    end
+  end
+end
+
+class C
+  def a_method
+    'C#a_method()'
+  end
+
+  class << self
+    def a_class_method
+      'C.a_class_method()'
+    end
+  end
+end
+
+class D < C
+end
+
+obj = D.new
+p obj.a_method
+
+class << obj
+  def a_singleton_method
+    'obj#a_singleton_method()'
+  end
+end
+
+p obj.eigenclass.superclass
+
+p C.eigenclass
+p D.eigenclass
+p D.eigenclass.superclass
+p C.eigenclass.superclass
+
+p D.a_class_method
