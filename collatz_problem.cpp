6a5ed621e11f0de8d63d65c95ca28ad940ad19a1
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-04-27 18:45:31 +0900

    Finish ~2.3

---
+++ collatz_problem.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    while (cin >> n, n) {
+        int ans = 0;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            ans++;
+        }
+        cout << ans << endl;
+    }
+}
