3095c34b649e772da900dd4ef4ea3c9b8c6bccdf
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2015-02-18 14:04:09 +0900

    APUE: chapter 14

---
+++ advanced-programming-in-the-unix-environment/c14/lock-test.c
@@ -0,0 +1,20 @@
+/* Figure 14.6 */
+#include "apue.h"
+#include <fcntl.h>
+
+pid_t lock_test(int fd, int type, off_t offset, int whence, off_t len) {
+    struct flock lock;
+
+    lock.l_type = type;
+    lock.l_start = offset;
+    lock.l_whence = whence;
+    lock.l_len = len;
+
+    if (fcntl(fd, F_GETLK, &lock) == -1) {
+        err_sys("fcntl error");
+    }
+    if (lock.l_type == F_UNLCK) {
+        return 0;
+    }
+    return lock.l_pid;
+}
---
+++ advanced-programming-in-the-unix-environment/c14/sleep-us-select.c
@@ -0,0 +1,11 @@
+/* Exercise 14.5, Figure C.16 */
+#include "apue.h"
+#include <sys/select.h>
+
+void sleep_us(unsigned int nusecs) {
+    struct timeval tval;
+
+    tval.tv_sec = nusecs / 1000000;
+    tval.tv_usec = nusecs % 1000000;
+    select(0, NULL, NULL, NULL, &tval);
+}
