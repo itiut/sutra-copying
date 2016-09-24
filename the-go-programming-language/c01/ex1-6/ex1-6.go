942777ac7599f3ad8ac4e85a5fb1c60484e26783
Yuichi Tanikawa <t.yuichi111@gmail.com>
2016-09-25 00:46:11 +0900

    The Go Programming Language: section 1.4

---
+++ the-go-programming-language/c01/ex1-6/ex1-6.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"path"
+	"strconv"
+)
+
+var palette = []color.Color{
+	color.White,
+	color.Black,
+	color.RGBA{0xff, 0x00, 0x00, 0xff},
+	color.RGBA{0x00, 0xff, 0x00, 0xff},
+	color.RGBA{0x00, 0x00, 0xff, 0xff},
+	color.RGBA{0xff, 0xff, 0x00, 0xff},
+	color.RGBA{0xff, 0x00, 0xff, 0xff},
+	color.RGBA{0x00, 0xff, 0xff, 0xff},
+}
+
+func main() {
+	colorIndex := 1
+	if len(os.Args) > 1 {
+		idx, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "usage: %s colorIndex\n", path.Base(os.Args[0]))
+			return
+		}
+		if 0 < idx && idx < len(palette) {
+			colorIndex = idx
+		}
+	}
+	lissajous(os.Stdout, colorIndex)
+}
+
+func lissajous(out io.Writer, colorIndex int) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(colorIndex))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
