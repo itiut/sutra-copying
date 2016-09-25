e901ae2706b2dbad300c023dba3a1aab21742852
Yuichi Tanikawa <t.yuichi111@gmail.com>
2016-09-25 14:32:45 +0900

    The Go Programming Language: ~ section 1.6

---
+++ the-go-programming-language/c01/ex1-9/ex1-9.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		fmt.Printf("Request: GET %s\n", url)
+		fmt.Printf("Status: %s\n", resp.Status)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
