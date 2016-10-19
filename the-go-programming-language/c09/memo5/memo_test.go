ae4593b9ccff16a000bd7921011875f501980cec
Yuichi Tanikawa <t.yuichi111@gmail.com>
2016-10-19 13:12:28 +0900

    The Go Programming Language: section 9.7

---
+++ the-go-programming-language/c09/memo5/memo_test.go
@@ -0,0 +1,23 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package memo_test
+
+import (
+	"testing"
+
+	"github.com/itiut/sutra-copying/the-go-programming-language/c09/memo5"
+	"github.com/itiut/sutra-copying/the-go-programming-language/c09/memotest"
+)
+
+var httpGetBody = memotest.HTTPGetBody
+
+func Test(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Sequential(t, m)
+}
+
+func TestConcurrent(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Concurrent(t, m)
+}
