e2e079bb37182e8ba7af290c931238b0a092b88c
Yuichi Tanikawa <t.yuichi111@gmail.com>
2016-10-31 19:52:33 +0900

    goblueprints: chapter 1

---
+++ goblueprints/c01/chat/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/itiut/sutra-copying/goblueprints/c01/chat/trace"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	addr := flag.String("addr", ":8080", "address for the application")
+	flag.Parse()
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
+	log.Println("start web server. port: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
---
+++ goblueprints/c01/chat/room.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/itiut/sutra-copying/goblueprints/c01/chat/trace"
+)
+
+type room struct {
+	forward chan []byte
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+	tracer  trace.Tracer
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+		tracer:  trace.Off(),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// join
+			r.clients[client] = true
+			r.tracer.Trace("a new client joined")
+
+		case client := <-r.leave:
+			// leave
+			delete(r.clients, client)
+			close(client.send)
+			r.tracer.Trace("a client left")
+
+		case msg := <-r.forward:
+			r.tracer.Trace("received message: ", string(msg))
+			// forward message to all clients
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// succeeded in sending message
+					r.tracer.Trace(" -- sent to client")
+				default:
+					// failed to send message
+					delete(r.clients, client)
+					close(client.send)
+					r.tracer.Trace(" -- failed to send. clean up the client")
+				}
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
